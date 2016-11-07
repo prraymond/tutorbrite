c80de886d893448629126a952462b5b64a6b6c68
Ceren Safak <ceren.safak@yale.edu>
2016-11-07 15:32:22 +0000

    Remove conflict cruft

--- controllers/index.js
+++ controllers/index.js
@@ -7,13 +7,8 @@ var events = require('../models/events');
  */
 function index (request, response) {
   var contextData = {
-<<<<<<< HEAD
     'title': 'Yale SOM tutoring sessions',
     'tagline': 'Learn talents from your classmates',
-=======
-    'title': 'Yale SOM tutoring site',
-    'tagline': 'You can use this platform to learn talent from your peers',
->>>>>>> 280281061a1bae8eb12ffef0e0b4937b0e524c28
     'events': events.all
   };
   response.render('index.html', contextData);
